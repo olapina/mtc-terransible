1d5f977aed4f0e70f7f14d57799f092cf801fb6b
Ubuntu <ubuntu@ip-172-31-41-31.us-east-2.compute.internal>
2022-02-25 22:06:40 +0000

    Added output section

--- compute.tf
+++ compute.tf
@@ -69,4 +69,12 @@ resource "aws_instance" "mtc_main" {
 #   }
 # }
 
+output "grafana_access" {
+  value = {for i in aws_instance.mtc_main[*] : i.tags.Name => "${i.public_ip}:3000"}
+}
+
+output "test" {
+  value = "testing"
+}
+
 
